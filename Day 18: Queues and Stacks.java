90f37c683e45de2ae33d3bc3f55bfdf53b01adca
Mayuri Narute <66169874+MayuriNarute@users.noreply.github.com>
2023-03-30 21:24:48 +0530

    Create Day 18: Queues and Stacks.java

---
+++ Day 18: Queues and Stacks.java
@@ -0,0 +1,17 @@
+LinkedList<Character> queue = new LinkedList();
+    LinkedList<Character> stack = new LinkedList();
+    
+    public void pushCharacter(char ch){
+        stack.push(ch);
+    }
+    
+    public void enqueueCharacter(char ch){
+        queue.add(ch);
+    }
+    
+    public char popCharacter(){
+        return stack.pop();
+    }
+     public char dequeueCharacter(){
+        return queue.remove();
+    }
